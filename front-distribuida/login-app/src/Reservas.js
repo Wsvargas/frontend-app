9b4df0573d95a99d389196d109f551e26eb8a403
Willian Vargas <willianvargas1995@hotmail.com>
2025-02-10 22:51:57 -0500

    change ips

--- front-distribuida/login-app/src/Reservas.js
+++ front-distribuida/login-app/src/Reservas.js
@@ -11,7 +11,7 @@ const Reservas = () => {
 
     // 🔹 Obtener vuelos disponibles con asientos y fecha válida
     useEffect(() => {
-        axios.get('http://13.216.49.242/flights')
+        axios.get('http://13.216.49.242:5032/flights')
             .then(response => {
                 const vuelosDisponibles = response.data.filter(flight => 
                     flight.status === "active" && 
